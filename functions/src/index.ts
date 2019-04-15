0a3b97f8de0d7a17c70108838a5daa9c7cf3af02
Dennis Alund <dennis.alund@gmail.com>
2019-04-15 11:27:30 +0800

    Updating app configuration

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,15 +3,18 @@ import * as tanam from 'tanam';
 
 tanam.initializeApp({
     users: {
-        "your.email@gmail.com": "owner",
+        "dennis@oddbit.id": "owner",
+        "dennis@kumpul.id": "owner",
+        "faye@kumpul.id": "owner",
+        "deta@oddbit.id": "admin",
     },
     firebaseApp: {
-        apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxx",
-        authDomain: "your-project-id.firebaseapp.com",
-        databaseURL: "https://your-project-id.firebaseio.com",
-        projectId: "your-project-id",
-        storageBucket: "your-project-id.appspot.com",
-        messagingSenderId: "0123456789"
+        apiKey: "AIzaSyDklPr1AZMlUNnZF_JBiklHYRf4ZT1us8A",
+        authDomain: "ruangtempo-dev.firebaseapp.com",
+        databaseURL: "https://ruangtempo-dev.firebaseio.com",
+        projectId: "ruangtempo-dev",
+        storageBucket: "ruangtempo-dev.appspot.com",
+        messagingSenderId: "681682867439"
     },
 });
 
